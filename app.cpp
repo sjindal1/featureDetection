bdc9d065b932c37fc3f0ea1188759300dfb7003d
Sahil Jindal <sahil.jindal@stonybrook.edu>
2018-08-03 10:50:30 -0400

    object detection

--- app.cpp
+++ app.cpp
@@ -9,6 +9,8 @@
 #include <opencv2/features2d.hpp>
 #include <opencv2/xfeatures2d.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/calib3d.hpp>
 
 using namespace cv;
 using namespace cv::xfeatures2d;
@@ -59,6 +61,8 @@ int main(int argc, char *argv[])
 
     Mat img_1 = imread(template_filename, IMREAD_GRAYSCALE); // Read the file
 
+    // bitwise_not ( img_1, img_1 );
+    
     if (!img_1.data)
     {
         std::cout << " --(!) Error reading template image " << std::endl;
@@ -106,10 +110,10 @@ int main(int argc, char *argv[])
         std::vector<cv::DMatch> good_matches;
         for (int i = 0; i < matches.size(); ++i)
         {
-            const float ratio = 0.65; // As in Lowe's paper; can be tuned
+            const float ratio = 0.75; // As in Lowe's paper; can be tuned
             if (matches[i].size() < 2)
                 continue;
-            if (matches[i][0].distance < ratio * matches[i][1].distance)
+            if (abs(matches[i][0].distance) < ratio * abs(matches[i][1].distance))
             {
                 good_matches.push_back(matches[i][0]);
             }
@@ -122,13 +126,44 @@ int main(int argc, char *argv[])
                         good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
                         std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
             //-- Show detected matches
-            imshow("Good Matches", img_matches);
+            // imshow("Good Matches", img_matches);
             // for( int i = 0; i < (int)good_matches.size(); i++ )
             // { printf( "-- Good Match [%d] Keypoint 1: %d  -- Keypoint 2: %d  \n", i, good_matches[i].queryIdx, good_matches[i].trainIdx ); }
+
+            //-- Localize the object
+            std::vector<Point2f> obj;
+            std::vector<Point2f> scene;
+
+            for( int i = 0; i < good_matches.size(); i++ )
+            {
+                //-- Get the keypoints from the good matches
+                obj.push_back( keypoints_1[ good_matches[i].queryIdx ].pt );
+                scene.push_back( keypoints_2[ good_matches[i].trainIdx ].pt );
+            }
+
+            Mat H = findHomography( obj, scene, CV_RANSAC );
+
+            //-- Get the corners from the image_1 ( the object to be "detected" )
+            std::vector<Point2f> obj_corners(4);
+            obj_corners[0] = cvPoint(0,0); obj_corners[1] = cvPoint( img_1.cols, 0 );
+            obj_corners[2] = cvPoint( img_1.cols, img_1.rows ); obj_corners[3] = cvPoint( 0, img_1.rows );
+            std::vector<Point2f> scene_corners(4);
+
+            perspectiveTransform( obj_corners, scene_corners, H);
+
+            //-- Draw lines between the corners (the mapped object in the scene - image_2 )
+            line( img_matches, scene_corners[0] + Point2f( img_1.cols, 0), scene_corners[1] + Point2f( img_1.cols, 0), Scalar(0, 255, 0), 4 );
+            line( img_matches, scene_corners[1] + Point2f( img_1.cols, 0), scene_corners[2] + Point2f( img_1.cols, 0), Scalar( 0, 255, 0), 4 );
+            line( img_matches, scene_corners[2] + Point2f( img_1.cols, 0), scene_corners[3] + Point2f( img_1.cols, 0), Scalar( 0, 255, 0), 4 );
+            line( img_matches, scene_corners[3] + Point2f( img_1.cols, 0), scene_corners[0] + Point2f( img_1.cols, 0), Scalar( 0, 255, 0), 4 );
+
+            //-- Show detected matches
+            imshow( "Good Matches & Object detection", img_matches );
+
         }
         catch (...)
         {
-            imshow("Good Matches", img_2);
+            imshow("Good Matches & Object detection", img_2);
         }
 
         char key = waitKey(0);
